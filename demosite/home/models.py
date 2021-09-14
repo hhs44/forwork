2bc5d3b71154c227021d981950c5316605e4106b
WGSHuang <1159986871@qq.com>
2021-09-14 23:16:25 +0800

    上传了明天可看的资料

--- demosite/home/models.py
+++ demosite/home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from wagtail.core import rich_text
 
 from wagtail.core.models import Page
-
+from wagtail.core.fields import *
+from wagtail.admin.edit_handlers import *
 
 class HomePage(Page):
-    pass
+    rich_text = RichTextField(bank=True)
+    stream_text = StreamField(bank=True)
+    
+    # 使得该字段可以在
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname='full')
+    ]
