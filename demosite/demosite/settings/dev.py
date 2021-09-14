40e991234c77dca1a70f2d45a0292b03ccc89b97
WGSHuang <1159986871@qq.com>
2021-09-14 22:09:19 +0800

    init project

---
+++ demosite/demosite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^1sffo5ujlqq$g0*j-xs&pmq(yim6+!4w%9bo9i9$o_ajgmh5e'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
